a2f945ee0f8c5687c87ba34f231c54ec77dbd9da
Muhammad Saqib <saqibhakeem45@gmail.com>
2025-10-20 11:55:03 +0500

    deployment fix

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from streamlit_webrtc import webrtc_streamer, VideoTransformerBase
 from ultralytics import YOLO
 
 # Absolute path to your model
-MODEL_PATH = r"D:\Projects\AI-ML\Ciggarrete and Vape Detection\best.pt"
+MODEL_PATH = r"\best.pt"
 
 # Load YOLO model once
 model = YOLO(MODEL_PATH)
